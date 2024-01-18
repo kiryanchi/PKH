2dedf7bc7993076f57a4d7f3a814c4d9f3592a7d
kiryanchi <kiryanchi@naver.com>
2024-01-18 16:37:02 +0900

    feat: Version Check

--- src/main/kotlin/com/whaleajang/action/common/CommonController.kt
+++ src/main/kotlin/com/whaleajang/action/common/CommonController.kt
@@ -1,11 +1,15 @@
 package com.whaleajang.action.common
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class CommonController {
 
-    @GetMapping
+    @GetMapping("/health")
     fun healthCheck(): String = "UP"
+
+    @GetMapping("/version")
+    fun versionCheck(@Value("\${Version}") version: String): String = version
 }
