0ba2b1faf05bcd748d63f6cc5828b4bfe0916efb
kiryanchi <kiryanchi@naver.com>
2024-01-19 12:22:41 +0900

    feat: Deploy to EC2

--- run.sh
+++ run.sh
@@ -2,53 +2,47 @@
 
 IS_GREEN=$(docker container ls | grep green)
 
-if [ $IS_GREEN ]; then # Current container: green
+if [ $IS_GREEN ]; then # Current container: green or none
 
-  echo "#### Green -> Blue ####"
+  echo "#### Turn on blue container ####"
 
-  echo "1. Build Spring"
-  ./gradlew bootJar
-
-  echo "2. Deploy spring as blue container"
+  echo "1. Deploy spring as blue container"
   docker compose up -d blue --build
 
-  echo "3. Wait until blue container turn on"
+  echo "2. Wait until blue container turn on"
   while [ 1 = 1 ]; do
     sleep 2
 
     echo "    Health Check for blue container"
     STATUS=$(curl -s http://127.0.0.1:8081/health)
     if [ $STATUS ]; then
-      echo "    Successfully blue container turned on"
+      echo "Successfully blue container turned on"
       break;
     fi
   done;
 
-  echo "#### Turn off green Container"
+  echo "#### Turn off if there is green Container"
   docker container stop green
 
 else # Current container: blue
 
-  echo "#### Blue -> Green ####"
+  echo "#### Turn on green container ####"
 
-  echo "1. Build Spring"
-  ./gradlew bootJar
-
-  echo "2.Deploy spring as green container"
+  echo "1.Deploy spring as green container"
   docker compose up -d green --build
 
-  echo "3. Wait until green container turn on"
+  echo "2. Wait until green container turn on"
   while [ 1 = 1 ]; do
     sleep 2
 
     echo "    Health Check for green container"
     STATUS=$(curl -s http://127.0.0.1:8080/health)
     if [ $STATUS ]; then
-      echo "    Successfully green container turned on"
+      echo "Successfully green container turned on"
       break;
     fi
   done;
 
-  echo "#### Turn off blue Container"
+  echo "#### Turn off if there is blue Container"
   docker container stop blue
 fi
